c9958890ac0ae21774c3b17824c0808f90fb4b74
LubHR <liubomyrhruzd89@gmail.com>
2024-07-03 22:45:52 +0300

    hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, {useReducer, useState} from 'react';
 import './App.css';
 
+const reduser = (state:number,action:{type:string,payload:number}) =>{
+    switch (action.type) {
+        case 'add':
+            return state + action.payload
+        case 'remove':
+            return state - action.payload
+
+    }
+    return state;
+}
+
 const App = () => {
+
+    const [counter, dispath] = useReducer(reduser,0);
+
     return (
         <div>
+            <h2>{counter}</h2>
+
+            <button onClick={() => {
+                dispath({type: 'add', payload: 10})
+            }}>
+                inc
+            </button>
+
+            <button onClick={() => {
+                dispath({type:'remove', payload:20})
+            }}>
+                dec
+            </button>
+
         </div>
     )
 }
